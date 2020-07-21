5c949a17c08aa988876ffbc164b13422e7a9eec0
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-07-22 00:04:54 +0530

    Day 20

---
+++ 20 July/Remove_Linked_list_element.cpp
@@ -0,0 +1,22 @@
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+    if(head == NULL)
+        return head;
+
+    struct ListNode *current = (struct ListNode*)calloc(1,sizeof(struct ListNode));
+    current -> next = head;
+    head = current;
+
+    while(current -> next)
+    {
+        if(current -> next -> val == val && current -> next -> next == NULL)
+            current -> next = NULL;
+
+        else if(current -> next -> val == val)
+            current -> next = current -> next -> next;
+
+        else
+            current = current -> next;
+    }
+    return head -> next;
+}
