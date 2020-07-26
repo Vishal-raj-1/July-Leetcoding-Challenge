6edadff7e0e9bd7fbc05a03e7cc01a751192ec11
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-07-26 13:45:02 +0530

    Day 25

---
+++ 25 July/Find_Minimum_in_Rotated_Sorted_Array_ll.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int findMin(vector<int>& nums)
+    {
+        int l = 0, h = nums.size() - 1;
+
+        while(l < h)
+        {
+            int m = (l+h)/2;
+
+            if(nums[m] > nums[h])
+                l = m+1;
+            else if(nums[m] < nums[h])
+                h = m;
+            else
+                h--;
+        }
+        return nums[l];
+    }
+};
